3f586e51d91bb3daa850febcf3ffcad8524705a9
Fedenko <mfedenko@follett.com>
2023-06-14 23:16:33 +0300

    Add CSRF protection and allow CORS for our front-end.

--- src/main/java/com/easy/bank/controller/ContactController.java
+++ src/main/java/com/easy/bank/controller/ContactController.java
@@ -3,7 +3,7 @@ package com.easy.bank.controller;
 import com.easy.bank.model.Contact;
 import com.easy.bank.repository.ContactRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +16,7 @@ public class ContactController {
 
     private final ContactRepository repository;
 
-    @GetMapping("/contact")
+    @PostMapping("/contact")
     public Contact saveContactInquiryDetails(@RequestBody Contact contact) {
         contact.setContactId(getServiceReqNumber());
         contact.setCreateDt(new Date(System.currentTimeMillis()));
